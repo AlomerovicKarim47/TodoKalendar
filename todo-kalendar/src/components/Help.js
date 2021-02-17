ae489147eff98bb7605bb4d577368559093f31c1
Karim Alomerovic <kalomerovi1@etf.unsa.ba>
2021-02-17 17:45:40 +0100

    Help section

--- todo-kalendar/src/components/Help.js
+++ todo-kalendar/src/components/Help.js
@@ -5,12 +5,13 @@ export default class Help extends Component {
     render() {
         return (
             <div>
-                <Modal show = {this.props.show}>
-                    <h2>Pomoć</h2>
-                    <hr/>
+                <Modal show = {this.props.show} dialogClassName="help-modal">
+                    <h2>Pomoć</h2><hr/>
+                    
                     <div style = {{height:'600px', overflowY:'scroll', marginBottom:'15px'}}>
-        
+                        <hr/>
                         <h3>Vremenska crta-kalendar</h3>
+                        <hr/>
                         <h5>Nalazi se sa desne strane i zauzima većinu ekrana. Na njoj su raspoređeni vaši zadaci po danu i vremenu. Vremenska crta se nalazi sa lijeve, a datumi i dani u sedmici sa gornje strane.</h5>
                         <h4>Nazad i naprijed</h4>
                         <h5>Pomjera kalendar za jedan dan u budućnost ili prošlost.</h5>
@@ -20,8 +21,12 @@ export default class Help extends Component {
                         </h5>
 
                         <hr/>
+                        
                         <h3>Meni</h3>
+                        <hr/>
+                        <h5>Nalazi se sa lijeve strane i sadrži opcije za upravljanje zadacima i vremenskom crtom-kalendarom.</h5>
                         <h4>Mini kalendar</h4>
+                        
                         <h5>Služi za navigaciju vermenskom crtom-kalendarom. Klikom na datum taj datum postavljamo na prvo mjesto na vremenskoj crti-kalendaru. 
                             Klikom na "Today" se vraćamo na današnji datum. Klikom na dugme sa mjesecom i godinom sa lijeve strane mijenjamo izgled kalendara (različit izgled omogućava drugačije i brže kretanje kroz kalendar).</h5>
                         <h4>Pretraga</h4>
@@ -41,6 +46,14 @@ export default class Help extends Component {
                         <h5>Otvara dijalog sa poljem za unos koda za dijeljenje zadataka. Zalijepite kod za dijeljenje koji ste dobili kako biste importovali zadatak u vlastiti raspored.</h5>
                         <h4>Odjava</h4>
                         <h5>Odjavljuje vas sa sistema i vraća na ekran za prijavu.</h5>
+                        <hr/>
+                        <h3>Kategorije</h3>
+                        <hr/>
+                        <h5>Aplikacija nudi mogućnost dodavanja i brisanja kategorija zadataka.</h5>
+                        <h4>Dodavanje kategorije</h4>
+                        <h5>Vrši se klikom na Dodaj ili Izmjeni i klikom na + dugme pored polja za biranje kategorije. Otvara se dijalog gdje unosite naziv nove kategorije.</h5>
+                        <h4>Brisanje kategorije</h4>
+                        <h5>Vrši se klikom na "x" pored naziva kategorije u drop down meniju polja za odabir kategorije.</h5>
                     </div>
                     <Button onClick = {() => this.props.closeModal("help")}>Nazad</Button>
                 </Modal>

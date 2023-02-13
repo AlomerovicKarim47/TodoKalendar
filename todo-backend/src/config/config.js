6b041fb7225da15fd1c48af57aaa12fbd2c24a6e
alomerovickarim47 <karimalomerovic@yahoo.com>
2023-02-13 19:58:09 +0100

    ssl

--- todo-backend/src/config/config.js
+++ todo-backend/src/config/config.js
@@ -8,7 +8,7 @@ const CONFIG  = {
     },
     connectionString: process.env.NODE_ENV=="development"?
             `postgres://root:root@localhost:5432/todokalendar`:
-            "postgres://root:J0MctfgHElCuVcJIJG37gZFcW8hUoRu5@dpg-cfl7nu5a49903fk9nc5g-a.frankfurt-postgres.render.com/todokalendar",
+            "postgres://root:J0MctfgHElCuVcJIJG37gZFcW8hUoRu5@dpg-cfl7nu5a49903fk9nc5g-a.frankfurt-postgres.render.com/todokalendar?ssl=true",
     allowedOrigin: process.env.NODE_ENV=="development"?
             "http://localhost:3000":
             "https://todokalendar.onrender.com"

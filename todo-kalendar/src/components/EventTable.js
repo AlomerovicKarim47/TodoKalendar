608647447a7054c14e03a3639c628da003aef394
alomerovickarim47 <karimalomerovic@yahoo.com>
2023-03-23 13:22:37 +0100

    Update

--- todo-kalendar/src/components/EventTable.js
+++ todo-kalendar/src/components/EventTable.js
@@ -1,6 +1,6 @@
 import Timetable from 'react-timetable-events'
 import React, { Component } from 'react'
-import {Button, Container, Row, Col} from 'react-bootstrap'
+import {Button} from 'react-bootstrap'
 import Event from './Event'
 import '../css/EventTable.css'
 
@@ -10,10 +10,10 @@ export default class EventTable extends Component {
         dani:[]
     }
 
-    handleResize = ()=>{
+    handleResize = ()=>{  
         let dani = new Map()
         let windowSize = window.innerWidth;
-        let condition = windowSize > 768;
+        let condition = windowSize > 900;
         dani['Monday']=condition?'ponedjeljak':'pon'
         dani['Tuesday']=condition?'utorak':'uto'
         dani['Wednesday']=condition?'srijeda':'sri'
@@ -24,20 +24,67 @@ export default class EventTable extends Component {
         this.setState({dani:dani})
     }
 
+    dayAppend = (e) => { 
+        if (window.innerWidth > 768) return
+        let dani = new Map()
+        dani['pon']='ponedjeljak';
+        dani['uto']='utorak';
+        dani['sri']='srijeda';
+        dani['cet']='cetvrtak'; 
+        dani['pet']='petak';
+        dani['sub']='subota';
+        dani['ned']='nedjelja';
+        let element = e.target;
+        let expansion = document.createElement("div")
+        expansion.className = "day-label-expansion"
+        expansion.innerHTML = `<span>${element.dataset.datum}</span>
+                                <span>(${dani[element.dataset.dan]})</span>`;
+        element.appendChild(expansion)
+    }
+    dayRemove = (e) => {
+        if (window.innerWidth > 768) return
+        let element = e.target;
+        element.removeChild(element.lastChild)
+    }
+
+    addDayListeners(){
+        let days = document.getElementsByClassName("day-label") 
+        Array.prototype.forEach.call(days, (day) => {
+            day.addEventListener("mouseenter", this.dayAppend)
+            day.addEventListener("mouseleave", this.dayRemove)
+        })
+    }
+
+    removeDayListeners(){
+        let days = document.getElementsByClassName("day-label") 
+        Array.prototype.forEach.call(days, (day) => {
+            day.removeEventListener("mouseenter", this.dayAppend)
+            day.removeEventListener("mouseleave", this.dayRemove)
+        })
+    }
+
     componentDidMount(){
+        this.addDayListeners()
         window.addEventListener("resize", this.handleResize)
         this.handleResize()
     }
 
+    componentDidUpdate(){
+        this.addDayListeners()
+    }
+
     componentWillUnmount(){
+        this.removeDayListeners()
         window.removeEventListener("resize", this.handleResize)
     }
 
     renderDayLabel = (day) => {
         let data = day.split(" ")
         let datum = data[0].split("-")
-        return (window.innerWidth > 768?datum[2] + "/" + datum[1] + "/" + datum[0]:"")  
-        + " (" + this.state.dani[data[1]] + ")"
+        return <div className = "day-label" data-dan = {this.state.dani[data[1]]} data-datum = {datum[2] + "/" + datum[1] + "/" + datum[0]}>
+                    <span>{(window.innerWidth > 768?datum[2] + "/" + datum[1] + "/" + datum[0]:"")}</span>
+                    <span>{" (" + this.state.dani[data[1]] + ")"}</span>
+                </div>
     }
 
     renderEvent = (event, defaultAttributes, styles) => {
